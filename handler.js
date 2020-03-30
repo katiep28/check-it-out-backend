be3ab3af5627d9cf8736c7f0e1bce15534ab2e4d
Katie Postle <katiepostle@hotmail.co.uk>
2020-03-30 22:36:41 +0100

    First api working

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+
 
 const app = express();
+
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -13,15 +15,15 @@ const connection = mysql.createConnection({
   database: "checkitout"
 });
 
-
 app.use(cors());
 app.use(bodyParser.json());
 
-
-itemname
+app.get("/tasks", function(req, res) {
+  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
 
 app.get("/itemname", function(req, res) {
-  console.log("Hello");
+ 
     connection.query("SELECT name FROM item ",
                     function(err, data) {
       if (err) {
@@ -31,54 +33,42 @@ app.get("/itemname", function(req, res) {
         });
       } else {
         res.json({
-          itemlist: data
+          itemData: data
         });
       }
     });
-  
   });
   
+// app.get("/itemstatus/:itemid/:location", function(req, res) {
 
-app.get("/itemstatus/:itemid/:location", function(req, res) {
+// const itemid = req.params.itemid;
+// const location = req.params.location;
 
-const itemid = req.params.itemid;
-const location = req.params.location;
-
-  connection.query("SELECT store.name, " +
-                          "item_store_location.quantity, " +
-                          "item_store_location.dateupdated " +
-                   "FROM item_store_location " + 
-                   "INNER JOIN store ON item_store_location.storeid=store.id " +
-                   "WHERE item_store_location.itemid = ? "+
-                   "AND item_store_location.postcode= ?"
-                [itemid, location],
-                function(err, data) {
-                  if (err) {
-                      console.log("Error fetching item", err);
-                      res.status(500).json({
-                        error: err
-                      }); 
-                  }
-                  else {
-                      res.json({
-                        itemStatusList: data
-                      });
-                  }
-                  });
-          });
-
-
-// app.post("/addstatus/:item/:store/:status", function(req, res) {
-//   const name = req.body.name;
-//   const alcoholic = req.body.alcoholic;
-//   const drinks = req.body.drinks;
-//   const recipe = req.body.recipe;
-
-//   res.json({
-//     message: "Received a request to add a cocktail " + name
-//   });
-// });
+// connection.query("SELECT store.name, " +
+//                           "item_store_location.quantity, " +
+//                           "item_store_location.dateupdated " +
+//                    "FROM item_store_location " + 
+//                    "INNER JOIN store ON item_store_location.storeid=store.id " +
+//                    "WHERE item_store_location.itemid = ? " +
+//                    "AND item_store_location.postcode= ? ",
+//                 [itemid, location],
+//                 function(err, data) {
+//                   if (err) {
+//                       console.log("Error fetching item status", err);
+//                       res.status(500).json({
+//                         error: err
+//                       }); 
+//                   }
+//                   else {
+//                       res.json({
+//                         itemData: data
+//                       });
+//                   }
+//                   });
+//           });
 
 
 
-module.exports.checkitout = serverless(app);
+module.exports.items = serverless(app);
+
+
